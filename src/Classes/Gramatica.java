9cfe1d572da7ddc87bc7d278c374aac31aefed0d
Daniel Blanco <danielblancob48@gmail.com>
2015-11-16 14:06:24 -0500

    first commit

---
+++ src/Classes/Gramatica.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Daniel
+ */
+
+public class Gramatica {
+    ArrayList<Produccion> producciones;
+    ArrayList<NonTerminal> NTs; 
+    
+    public Gramatica(){
+        
+    }
+}
